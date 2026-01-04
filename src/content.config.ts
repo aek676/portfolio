e6bab3c1a7bba415b7561f4c1363b55709e18b37
aek676 <aek676@inlumine.ual.es>
2026-01-04 18:20:15 +0100

    algo

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,15 @@
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
-import { defineCollection } from 'astro:content';
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+import { defineCollection } from "astro:content";
+import type { Tag } from "@/types";
 
 const projects = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.mdx', base: './src/projects' }),
+  loader: glob({ pattern: "**/[^_]*.mdx", base: "./src/projects" }),
   schema: z.object({
     title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    tags: z.array(z.custom<Tag>()),
   }),
 });
 
